479df4018606a2676d8ed1056d8560db1358b45e
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2022-11-07 08:02:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple-Gravestones
+ This mod adds gravestones which spawn after a players has died.
---
+++ src/main/kotlin/de/mrcreativ3001/simplegravestones/SimpleGravestones.kt
@@ -0,0 +1,11 @@
+package de.mrcreativ3001.simplegravestones
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object SimpleGravestones: ModInitializer {
+    private const val MOD_ID = "simplegravestones"
+
+    override fun onInitialize() {
+        
+    }
+}
